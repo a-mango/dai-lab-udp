923b3ab2628bbc799e6d83ecd1f68b9cc5d08884
acm <a-mango@users.noreply.github.com>
2024-01-24 23:38:25 +0100

    build: maven setup

---
+++ docker/image-auditor/auditor/src/main/java/ch/heig/dai/lab/udp/auditor/Main.java
@@ -0,0 +1,17 @@
+package ch.heig.dai.lab.udp.auditor;
+
+/**
+ *
+ *
+ * @author Aubry Mangold <aubry.mangold@heig-vd.ch>
+ * @author Hugo Germano <hugo.germano@heig-vd.ch>
+ */
+public class Main {
+
+    /**
+     * The main entry point of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
